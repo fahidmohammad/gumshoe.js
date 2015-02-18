c52a47b29523c110cc04a763b83ebfd75252d14c
Andrew Vaughan <andrew.vaughan@hyatt.com>
2015-02-18 12:54:28 -0600

    Added Apache 2.0 license

--- src/gumshoe.js
+++ src/gumshoe.js
@@ -1,5 +1,21 @@
-/*! Gumshoe v0.1.0 | (c) 2015 Hyatt Hotels Corporation | @license http://www.apache.org/licenses/LICENSE-2.0 */
-
+/*! Gumshoe v1.0.0
+ *
+ * Copyright 2015 Hyatt Hotels Corporation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * 
+ *  @license http://www.apache.org/licenses/LICENSE-2.0
+ */
 (function() {
 	
 	/**
