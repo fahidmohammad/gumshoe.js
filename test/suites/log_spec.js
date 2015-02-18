026d5af18b3d73af932bd1c8944c9bc861771b76
Andrew Vaughan <andrew.vaughan@hyatt.com>
2015-02-18 12:44:57 -0600

    Added console reporting tests

--- test/suites/log_spec.js
+++ test/suites/log_spec.js
@@ -1,4 +1,4 @@
-describe("Gumshoe logging functions", function() {
+describe("Logging functions", function() {
 
 	beforeEach(function() {
 		Gumshoe.debug = true;
@@ -9,7 +9,54 @@ describe("Gumshoe logging functions", function() {
 	});
 
  	it ("should send log messages to the console", function() {
- 		
+ 		spyOn(console, "log");
+
+ 		Gumshoe.logInfo("test log");
+
+ 		expect(console.log)
+ 			.toHaveBeenCalledWith("test log");
  	});
+
+ 	it ("should send warning messages")
+
+});
+
+
+describe("Debug state", function() {
+
+	it ("should not log when debugging is off", function() {
+		expect(Gumshoe.debug)
+			.toBeFalsy();
+
+		spyOn(console, "log");
+		
+		Gumshoe.logInfo("info");
+
+		expect(console.log)
+			.not
+			.toHaveBeenCalled();
+	});
+
+	it ("should not warn when debugging is off", function() {
+		expect(Gumshoe.debug)
+			.toBeFalsy();
+
+		spyOn(console, "warn");
+		
+		Gumshoe.logWarning("source", "warning");
+
+		expect(console.warn)
+			.not
+			.toHaveBeenCalled();
+	});
+
+	it ("should not throw Errors when debugging is off", function() {
+		expect(Gumshoe.debug)
+			.toBeFalsy();
+
+		expect(function() { Gumshoe.logError("source", "error") })
+			.not
+			.toThrow();
+	});
 
 });
